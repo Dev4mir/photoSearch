563836d18cf89e70bbad485417c9db8cadae6a88
Dev4mir <amir.labib91@gmail.com>
2017-05-13 14:56:54 +0200

    Let's see

--- src/app/componentes/photos/photos.component.ts
+++ src/app/componentes/photos/photos.component.ts
@@ -20,38 +20,50 @@ export class PhotosComponent {
   page: number = 1;
   showSelected: boolean = false;
   searchArr: Array<string>=[];
-  showLoading: boolean = false;
+  showLoading: boolean = true;
 
   constructor(public flickrService: FlickrSearchService){ }
 
   onScroll(){
     this.page += 1;
-    console.log(this.page)
+    console.log(this.page);
     this.searchPhoto(this.page);
   }
-  loadingP(){
-    this.showLoading = true;
+  loadingP(s: boolean){
+    if(s === true){
+       this.showLoading = true;
+    }else{
+      this.showLoading = false;
+    }
   }
-  public a;
+
+  public a: string;
   onSelectImg(pho){
     this.a = pho.replace(/_q.jpg/, '_b.jpg');
     this.showSelected = true;
   }
 
   getData(d){
-    d['photos']['photo'].forEach(photo => {
-      this.photos.push(new Photo(
-        photo.id,
-        photo.server,
-        photo.farm,
-        photo.secret,
-        photo.owner
-      ));
-    })
+    this.loadingP(false);
+    if(d.photos.photo.length === 0){
+      this.onScroll();
+    }else{
+      d.photos.photo.forEach(photo => {
+        this.photos.push(new Photo(
+          photo.id,
+          photo.server,
+          photo.farm,
+          photo.secret,
+          photo.owner
+        ));
+      })
+    }
+
   }
 
    searchPhoto(p?: number){
-      this.loadingP();
+     var elm = document.getElementById('scrollDiv');
+
      var url = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + this.api_key + '&text=' + this.searchStr + '&per_page=56' + '&page=' + p + '&format=json&nojsoncallback=1';
      if(this.searchStr === ''){
        this.show = false;
@@ -61,19 +73,20 @@ export class PhotosComponent {
          data => {
 
            if(p === undefined){
-             this.photos = []
+             this.photos = [];
+             this.page = 1;
              this.getData(data);
+             elm.scrollTop = 10;
            }else{
              this.getData(data);
            }
-         },
-         err => { console.log(err)},
-         () => {this.showLoading = false;}
-       );
 
+         },
+         err => { console.log(err)}
+       );
      }
 
+     this.loadingP(true);
+
   }
-
-
 }
